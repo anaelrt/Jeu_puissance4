88c2022b52228d5a46763d14fac9830ef74a713c
Anaé Lrt <leurette.anae@gmail.com>
2026-03-16 14:22:57 +0100

    matrice essai

--- jeu_p4.py
+++ jeu_p4.py
@@ -1,5 +1,15 @@
 "jeu_p4"
-a=2
-b=3
-c=4
-d=5
+#a=2
+#b=3
+#c=4
+#d=5
+
+m=int(input("nombre de lignes"))
+n=int(input("nombre de colonnes"))
+matrice=[]
+for ligne_index in range(m):
+    ligne=[]
+    for colonne_index in range(n):
+        valeur=int(input)
+        ligne.append(valeur)
+    matrice.append
