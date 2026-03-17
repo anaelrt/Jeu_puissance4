e53743f0bfd5fda36359b7ca3b2f39b9510190cd
Anaé Lrt <leurette.anae@gmail.com>
2026-03-17 17:48:40 +0100

    ordre des joueurs

---
+++ #test projet.py
@@ -0,0 +1,8 @@
+#test projet
+import random
+choix_joueur=random.randint(1, 2)
+print(choix_joueur)
+if choix_joueur == 1:
+    print("tu commences")
+else:
+    print()
--- jeu_p4.py
+++ jeu_p4.py
@@ -1,8 +1,13 @@
-import random
 
+
+import random                                        #choix de l'ordre du joueur
 choix_joueur=random.randint(1, 2)
+print(choix_joueur)
 if choix_joueur == 1:
     print("tu commences")
+else:
+    print("tu ne commences pas à jouer en premier")
+
 
 m=int(input("nombre de lignes"))
 n=int(input("nombre de colonnes"))
@@ -20,7 +25,7 @@ def addToken(joueur, colonne):
 
     
 
-test
+#test
 
 
 #print("--Puissance--")
