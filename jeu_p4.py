9851f1689604e7c951e8515bb6d8afdd3015cafa
Anaé Lrt <leurette.anae@gmail.com>
2026-04-05 00:00:16 +0200

    pion colonne

--- jeu_p4.py
+++ jeu_p4.py
@@ -15,8 +15,8 @@ else:
     pion_joueur ="X"
     print("tu as les pions X")
 
-m=int(input("donner un nombre de ligne"))    #matrice de depart
-n=int(input("donner un nombre de colonne"))
+m=int(input("donner un nombre de ligne : "))    #matrice de depart
+n=int(input("donner un nombre de colonne : "))
 matrice=[]
 for ligne_index in range(m):
     ligne=[]
@@ -32,19 +32,18 @@ def affiche_matrice(matrice):
 affiche_matrice(matrice)
 
 
+
 choix_colonne=int(input("dans quelle colonne voulez vous mettre le pion ? "))   #ca fonctionne pas
 while choix_colonne<0 or choix_colonne>6:
     print("cette colonne est inexistante")
-    choix_colonne=int(input("ecrire une colonne entre 0 et 6 : "))
-else:
-    for i in range(43):
-       while choix_colonne == " ":
-            if ordre_joueur == 1:
-                matrice.append("O")
-            else:
-                matrice.append("X")
+    choix_colonne = int(input("Écrire une colonne entre 0 et {n-1} : "))
 
-            
+for ligne in range(m-1, -1, -1):   #mettre le pion dans le plus bas de la colonne
+    if matrice[ligne][choix_colonne]==" ":
+        matrice[ligne][choix_colonne]=pion_joueur
+    print(matrice)
+else:
+    print("la colonne est pleine ")
 
 
 #TEST STEEVEN 
