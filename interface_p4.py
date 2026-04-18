bc4fc27c65e1fed620ed0cc4bff8f9dc468f8ea5
Anaé <leurette.anae@gmail.com>
2026-04-18 21:47:45 +0200

    remis dans l'ordre et essai ihm

--- interface_p4.py
+++ interface_p4.py
@@ -1,10 +1,29 @@
-import tkinter 
+import tkinter as tk
 import random
 
-lignes=6
-colonnes=7
-
-fenetre=tkinter.Tk()
+fenetre = tk.Tk()
+fenetre.geometry("1600x1000")
 fenetre.title("puissance 4")
+
+label_nb_ligne=tk.Label(fenetre, text="Nombres de lignes", background="pink")        #labels
+label_nb_colonne=tk.Label(fenetre, text="Nombres de colonnes", background="pink")
+
+entry_ligne=tk.Entry(fenetre)     #champs de saisie
+entry_colonne=tk.Entry(fenetre)
+
+bouton_ok=tk.Button(fenetre, text="OK")    #bouton
+bouton_ok.bind("<Button-3>")
+
+label_nb_ligne.grid(row=0, column=0, sticky="e", padx=5, pady=5)          #placement des widgets
+entry_ligne.grid(row=0, column=1, columnspan=2, sticky="ew", padx=5, pady=5)
+
+label_nb_colonne.grid(row=1, column=0, sticky="e", padx=5, pady=5)  
+entry_colonne.grid(row=1, column=1, columnspan=2, sticky="ew", padx=5, pady=5)
+
+bouton_ok.grid(row=2, column=1, sticky="e", padx=20, pady=20) 
+
+fenetre.grid_columnconfigure(2, weight=10)          #ajuster la taille des colonnes
+fenetre.grid_rowconfigure(5, weight=5)
 fenetre.mainloop()
+
 
--- jeu_p4.py
+++ jeu_p4.py
@@ -30,137 +30,115 @@ tour_joueur=1
 
 m=int(input("donner un nombre de ligne : "))    #matrice de depart
 n=int(input("donner un nombre de colonne : "))
-matrice=[]
-for ligne_index in range(m):
-    ligne=[]
-    for colonne_index in range(n):
-        valeur=str(' ')
-        ligne.append(valeur)
-    matrice.append(ligne)
+
+def creation_matrice():
+    matrice=[]
+    for ligne_index in range(m):
+        ligne=[]
+        for colonne_index in range(n):
+            valeur=str(' ')
+            ligne.append(valeur)
+        matrice.append(ligne)
+    return matrice
+
+matrice=creation_matrice()
 
 def affiche_matrice(matrice):
     for ligne in matrice:
         print(ligne)
 
-while 1:
-    while True:
-        print(affiche_matrice(matrice))
-        choix_colonne=int(input("dans quelle colonne voulez vous mettre le pion ? "))   
-        while choix_colonne<0 or choix_colonne>=n:
-            print("cette colonne est inexistante")
-            choix_colonne = int(input(f"donner une colonne entre 0 et {n-1} : "))
 
-        place_pion= False
-        for ligne in range(m-1, -1, -1):              #mettre le pion dans le plus bas de la colonne
-            if matrice[ligne][choix_colonne]==" ":
-                matrice[ligne][choix_colonne]=joueur_actuel
-                enregistrer_partie(joueur_actuel, choix_colonne)
-                place_pion= True                          
-                break
+def victoire(matrice):            #verif si il y a victoire
+    m = len(matrice)        # nombre de lignes
+    n = len(matrice[0])     # nombre de colonnes
 
-        if not place_pion:
-            print("la colonne est pleine")
-            continue
+    # horizontal 
+    for i in range(m):
+        for j in range(n - 3):
+            if (matrice[i][j] != " " and
+                matrice[i][j] == matrice[i][j+1] == matrice[i][j+2] == matrice[i][j+3]):
+                return matrice[i][j]
 
+    # vertical
+    for i in range(m - 3):
+        for j in range(n):
+            if (matrice[i][j] != " " and
+                matrice[i][j] == matrice[i+1][j] == matrice[i+2][j] == matrice[i+3][j]):
+                return matrice[i][j]
 
+    # diagonale vers le bas
+    for i in range(m - 3):
+        for j in range(n - 3):
+            if (matrice[i][j] != " " and
+                matrice[i][j] == matrice[i+1][j+1] == matrice[i+2][j+2] == matrice[i+3][j+3]):
+                return matrice[i][j]
 
-        def victoire(matrice):            #verif si il y a victoire
-            m = len(matrice)        # nombre de lignes
-            n = len(matrice[0])     # nombre de colonnes
-
-        # horizontal 
-            for i in range(m):
-                for j in range(n - 3):
-                    if (matrice[i][j] != " " and
-                        matrice[i][j] == matrice[i][j+1] == matrice[i][j+2] == matrice[i][j+3]):
-                        return matrice[i][j]
-
-        # vertical
-            for i in range(m - 3):
-                for j in range(n):
-                    if (matrice[i][j] != " " and
-                        matrice[i][j] == matrice[i+1][j] == matrice[i+2][j] == matrice[i+3][j]):
-                        return matrice[i][j]
-
-        # diagonale vers le bas
-            for i in range(m - 3):
-                for j in range(n - 3):
-                    if (matrice[i][j] != " " and
-                        matrice[i][j] == matrice[i+1][j+1] == matrice[i+2][j+2] == matrice[i+3][j+3]):
-                        return matrice[i][j]
-
-        # diagonale vers le haut
-            for i in range(3, m):
-                for j in range(n - 3):
-                    if (matrice[i][j] != " " and
-                        matrice[i][j] == matrice[i-1][j+1] == matrice[i-2][j+2] == matrice[i-3][j+3]):
-                        return matrice[i][j]
+    # diagonale vers le haut
+    for i in range(3, m):
+        for j in range(n - 3):
+            if (matrice[i][j] != " " and
+                matrice[i][j] == matrice[i-1][j+1] == matrice[i-2][j+2] == matrice[i-3][j+3]):
+                return matrice[i][j]
 
         
-            return None  # pas de gagnant
-    
-    
+    return None  # pas de gagnant              
 
+def sauvegarder_file():                                
+    file=open("jeu_p4.txt", "w")           #enregistrer la partie  
+    file.write("coups du joueur1 : " + str(tab1) + "\n")
+    file.write("Coups du joueur2 : " + str(tab2) + "\n") 
+    file.close()
 
-        gagnant = victoire(matrice)              
+while True:
+    affiche_matrice(matrice)
+    choix_colonne=int(input("dans quelle colonne voulez vous mettre le pion ? "))   
+    while choix_colonne<0 or choix_colonne>=n:
+        print("cette colonne est inexistante")
+        choix_colonne = int(input(f"donner une colonne entre 0 et {n-1} : "))
 
-        if gagnant:
-            print(affiche_matrice(matrice))
-            print("La partie est terminée")
-            print(f"Le joueur ayant les pions {gagnant} a gagné")
+    place_pion= False
+    for ligne in range(m-1, -1, -1):              #mettre le pion dans le plus bas de la colonne
+        if matrice[ligne][choix_colonne]==" ":
+            matrice[ligne][choix_colonne]=joueur_actuel
+            enregistrer_partie(joueur_actuel, choix_colonne)
+            place_pion= True                          
             break
 
-        if not gagnant:
-            if joueur_actuel==pion_joueur1:     #changer de joueur
-                joueur_actuel=pion_joueur2
-            else:
-                joueur_actuel=pion_joueur1
-        tour_joueur+=1
-    
-    print("voulez vous recommencer une partie ?")      #choix de recommencer une partie mais ya un probleme
-    print("oui : tapez 1")
-    print("non : tapez 2")
-    nvl_partie=int(input())
-    if nvl_partie==1:
-        print("on recommence")
-        matrice=[]
-        for ligne_index in range(m):
-            ligne=[]
-            for colonne_index in range(n):
-                valeur=str(' ')
-                ligne.append(valeur)
-            matrice.append(ligne)
+    if not place_pion:
+        print("la colonne est pleine")
         continue
-    elif nvl_partie!=2:
-        print("voulez vous recommencer une partie ?")      
+        
+    gagnant = victoire(matrice)
+        
+    if gagnant:
+        affiche_matrice(matrice)
+        print("La partie est terminée")
+        print(f"Le joueur ayant les pions {gagnant} a gagné")
+        sauvegarder_file()
+
+        print("voulez vous recommencer une partie ?")      #choix de recommencer une partie 
         print("oui : tapez 1")
         print("non : tapez 2")
-        nvl_partie=int(input()) 
-        if nvl_partie==1:
-            matrice=[]
-            for ligne_index in range(m):
-                ligne=[]
-                for colonne_index in range(n):
-                    valeur=str(' ')
-                    ligne.append(valeur)
-                matrice.append(ligne)
-        elif nvl_partie!=2:
-            j=0
-            while j!=2 and j==1:
-                nvl_partie=int(input())
-                j+=1
-        else:
-            print("la partie est terminée")
-            break
-    else:
-        print("arret du jeu")
-        break
-    
-    
-                                    
-file=open("jeu_p4.txt", "w")           #enregistrer la partie mais pas sure que ce soit ca 
-file.write("coups du joueur1 : " + str(tab1) + "\n")
-file.write("Coups du joueur2 : " + str(tab2) + "\n") 
+        nvl_partie=int(input())
 
-file.close()
+        if nvl_partie==1:
+            matrice=creation_matrice()
+            tab1.clear()
+            tab2.clear()
+            joueur_actuel=pion_joueur1
+            continue
+        else:
+            print("choix non valide, arret du jeu")
+            break
+
+        
+    if joueur_actuel==pion_joueur1:     #changer de joueur
+        joueur_actuel=pion_joueur2
+    else:
+        joueur_actuel=pion_joueur1
+    
+
+
+
+
 
